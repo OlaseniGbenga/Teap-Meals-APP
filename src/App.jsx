f342943f37165eac7442c227aa7dcb4f85845cc0
Olaseni Gbenga <63647607+OlaseniGbenga@users.noreply.github.com>
2023-02-06 05:31:53 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Favorites from './components/Favorites.jsx'
+import Meals from './components/Meals.jsx'
+import Modal from './components/Modal.jsx'
+import Search from './components/Search.jsx'
+
+
+export default function App() {
+  return (
+    <main>
+      {/* <Search/>*/} 
+      {/* <Favorites/>*/}
+      <Meals/>
+      {/* <Modal/>*/}
+    </main>
+  )
+}
---
+++ src/components/Meals.jsx
@@ -0,0 +1,19 @@
+import {useGlobalContext} from '../context'
+
+const Meals = () => {
+
+  const {meals} = useGlobalContext()
+    
+    
+  return (
+    <section>
+      {meals.map((singleMeal)=>{
+      console.log(singleMeal)
+      return <h4>single meal</h4>
+      })}
+    </section>
+    
+  )
+}
+
+export default Meals
